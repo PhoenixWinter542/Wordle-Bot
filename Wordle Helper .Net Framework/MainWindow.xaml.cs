6d1f179e749b3c13b9049d2e4a7f200cae7b214d
Phoenix Winter <phoenixwinter@gmail.com>
2024-10-22 01:51:10 -0700

    Improved usability of frontend

--- Wordle Helper .Net Framework/MainWindow.xaml.cs
+++ Wordle Helper .Net Framework/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Word_Analyzer;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace Wordle_Helper
 {
@@ -29,6 +31,11 @@ namespace Wordle_Helper
 		string connectionString;
 		string tableString = "english.dbo.words";
 		string columnString = "words";
+		List<Button> SubmitButtons = new List<Button>();
+
+		Color correct = (Color)ColorConverter.ConvertFromString("#6aaa64");
+		Color partial = (Color)ColorConverter.ConvertFromString("#c9b458");
+		Color wrong = (Color)ColorConverter.ConvertFromString("#787c7e");
 
 		List<List<short>> states = new List<List<short>>();
 
@@ -47,23 +54,44 @@ namespace Wordle_Helper
 					tmp.Background = new SolidColorBrush(Colors.Transparent);
 					tmp.Content = "";
 					tmp.Focusable = false;
-					tmp.Margin = new Thickness(10, 10, 10, 10);
+					tmp.Margin = new Thickness(3);
 					tmp.HorizontalAlignment = HorizontalAlignment.Center;
 					tmp.VerticalAlignment = VerticalAlignment.Center;
 					tmp.Width = 70;
-					tmp.FontSize = 50;
+					tmp.FontSize = 35;
+					tmp.FontWeight = FontWeights.DemiBold;
 					tmp.Height = 70;
 					tmp.Name = "b" + row.ToString() + (col - 1).ToString();
 					Grid.SetRow(tmp, row);
 					Grid.SetColumn(tmp, col);
 					tmp.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(ColorUp);
 					tmp.PreviewMouseRightButtonDown += new MouseButtonEventHandler(ColorDown);
+					tmp.PreviewMouseWheel += new MouseWheelEventHandler(MouseWheelChange);
 					buttonRow.Add(tmp);
 					Layout.Children.Add(tmp);
 					stateRow.Add(0);
 				}
 				buttons.Add(buttonRow);
 				states.Add(stateRow);
+			}
+
+			SubmitButtons.Add(b0);
+			SubmitButtons.Add(b1);
+			SubmitButtons.Add(b2);
+			SubmitButtons.Add(b3);
+			SubmitButtons.Add(b4);
+			SubmitButtons.Add(b5);
+		}
+
+		private void MouseWheelChange(object sender, MouseWheelEventArgs e)
+		{
+			if(0 < e.Delta)
+			{
+				ToggleButton((Button)sender, 1);
+			}
+			else if (0 > e.Delta)
+			{
+				ToggleButton((Button)sender, -1);
 			}
 		}
 
@@ -74,18 +102,22 @@ namespace Wordle_Helper
 				case 0:
 					btn.Background = new SolidColorBrush(Colors.Transparent);
 					btn.Foreground = new SolidColorBrush(Colors.Black);
+					btn.BorderBrush = new SolidColorBrush(Colors.Black);
 					break;
 				case 1:
-					btn.Background = new SolidColorBrush(Colors.SeaGreen);
+					btn.Background = new SolidColorBrush(correct);
 					btn.Foreground = new SolidColorBrush(Colors.White);
+					btn.BorderBrush = new SolidColorBrush(Colors.Transparent);
 					break;
 				case 2:
-					btn.Background = new SolidColorBrush(Colors.Goldenrod);
+					btn.Background = new SolidColorBrush(partial);
 					btn.Foreground = new SolidColorBrush(Colors.White);
+					btn.BorderBrush = new SolidColorBrush(Colors.Transparent);
 					break;
 				case 3:
-					btn.Background = new SolidColorBrush(Colors.SlateGray);
+					btn.Background = new SolidColorBrush(wrong);
 					btn.Foreground = new SolidColorBrush(Colors.White);
+					btn.BorderBrush = new SolidColorBrush(Colors.Transparent);
 					break;
 			}
 		}
@@ -104,25 +136,28 @@ namespace Wordle_Helper
 
 			//Update state	|	Won't go to 0
 			short tmp = (short)(states[row][col] + inc);
-			if (tmp < 1)	//Ensure 1 <= tmp
+			if (tmp < 1)    //Ensure 1 <= tmp
 				tmp = 3;
 			tmp--;      //Rescale to 0 <= tmp
 			tmp %= 3;   //Ensure 0 <= tmp <= 2
-			tmp++;		//Rescale to 1 <= tmp <= 3
+			tmp++;      //Rescale to 1 <= tmp <= 3
 			states[row][col] = tmp;
 
 			//Update color
 			UpdateButtonColor(btn, states[row][col]);
+			TestEnable();
 		}
 
 		private void ColorUp(object sender, EventArgs e)
 		{
 			ToggleButton((Button)sender, 1);
+			RemoveColorHighlight();
 		}
 
 		private void ColorDown(object sender, EventArgs e)
 		{
 			ToggleButton((Button)sender, -1);
+			RemoveColorHighlight();
 		}
 
 		private void MoveBack()
@@ -137,7 +172,112 @@ namespace Wordle_Helper
 			}
 			states[position.row][position.col] = 0;
 			UpdateButtonColor(buttons[position.row][position.col], 0);
+			buttons[position.row][position.col].BorderThickness = new Thickness(1);
 			buttons[position.row][position.col].Content = "";
+			TestEnable();
+		}
+
+		private bool StatesAssigned()
+		{
+			foreach (short state in states[position.row])
+			{
+				if (0 == state)
+					return false; //At least one letter does not have a state
+			}
+			return true;
+		}
+
+		private void GrayOut(Button btn)
+		{
+			btn.Foreground = new SolidColorBrush(Colors.LightGray);
+			btn.Background = new SolidColorBrush(Colors.WhiteSmoke);
+			btn.BorderBrush = new SolidColorBrush(Colors.LightGray);
+		}
+
+		private void DisableSubmit()
+		{
+			Button btn = SubmitButtons[position.row];
+
+			//Set text to submit
+			btn.Content = "Submit";
+
+			//Grey out current submit button
+			GrayOut(btn);
+		}
+
+		private void EnableSubmit()
+		{
+			Button btn = SubmitButtons[position.row];
+
+			//Set Text to submit
+			btn.Content = "Submit";
+
+			//Recolor current submit button
+			btn.Foreground = new SolidColorBrush(Colors.BlanchedAlmond);
+			btn.Background = new SolidColorBrush(Colors.Sienna);
+			btn.BorderBrush = new SolidColorBrush(Colors.BurlyWood);
+		}
+
+		private void EnableSuggest()
+		{
+			Button btn = SubmitButtons[position.row];
+
+			//Set text to suggest
+			btn.Content = "Suggest";
+
+			//Recolor current submit button
+			btn.Foreground = new SolidColorBrush(Colors.LightCyan);
+			btn.Background = new SolidColorBrush(Colors.CadetBlue);
+			btn.BorderBrush = new SolidColorBrush(Colors.LightBlue);
+		}
+
+		private void TestEnable()
+		{
+			if (maxRow <= position.row)
+				return;
+
+			//Determine if sumbit or suggest
+			if (0 == position.col)
+			{
+				EnableSuggest();
+			}
+			else if (maxCol == position.col)
+			{
+				//Only enable if all letters have states
+				if (false == StatesAssigned())
+				{
+					DisableSubmit();
+					return;
+				}
+
+				EnableSubmit();
+			}
+			else
+			{
+				DisableSubmit();
+			}
+		}
+
+		private void HideBorders()
+		{
+			for (int i = 0; i < maxCol; i++)
+			{
+				buttons[position.row][i].BorderBrush = new SolidColorBrush(Colors.Transparent);
+			}
+		}
+
+		private void NextSubmit()
+		{
+			//Hide previous button
+			Button btn = SubmitButtons[position.row - 1];
+			btn.Visibility = Visibility.Hidden;
+
+			//Make sure button is colored correctly
+			EnableSuggest();
+
+			//Make new button visible
+			if (position.row < maxRow)
+				SubmitButtons[position.row].Visibility = Visibility.Visible;
 		}
 
 		private void MoveForward()
@@ -146,6 +286,7 @@ namespace Wordle_Helper
 			{
 				position.col++;
 			}
+			TestEnable();
 		}
 
 		private void NextRow()
@@ -155,23 +296,62 @@ namespace Wordle_Helper
 				position.row++;
 				position.col = 0;
 				position.colorCol = 0;
+				NextSubmit();   //Needs to be run after position.row is updated
 			}
+		}
+
+		private void HighlightColorPos()
+		{
+			if (0 < buttons[position.row][position.colorCol].Content.ToString().Count())  //Don't highlight if no letter is present
+			{
+				Button btn = buttons[position.row][position.colorCol];
+				btn.BorderBrush = new SolidColorBrush(Colors.Black);
+				btn.BorderThickness = new Thickness(2);
+			}
+		}
+
+		private void RemoveColorHighlight()
+		{
+			buttons[position.row][position.colorCol].BorderThickness = new Thickness(1);
+			if(0 != states[position.row][position.colorCol])
+			{
+				buttons[position.row][position.colorCol].BorderBrush = new SolidColorBrush(Colors.Transparent);
+			}
+		}
+
+		private void MoveColorMarker(int row, int prevCol)
+		{
+			var borderColor = new SolidColorBrush(Colors.Transparent);
+
+			//Only hide border when button is colored
+			if (0 == states[row][prevCol])
+				borderColor = new SolidColorBrush(Colors.Black);
+
+			Button btn = buttons[position.row][prevCol];
+			btn.BorderBrush = borderColor;
+			btn.BorderThickness = new Thickness(1);
+
+			HighlightColorPos();
 		}
 
 		private void ColorPosLeft()
 		{
-			if(0 < position.colorCol)
+			if (0 < position.colorCol)
 			{
 				position.colorCol--;
+				MoveColorMarker(position.row, position.colorCol + 1);
 			}
 		}
 
 		private void ColorPosRight()
 		{
-			if(position.colorCol < position.col && position.colorCol + 1 < maxCol)
+			if (position.colorCol < position.col && position.colorCol + 1 < maxCol)
 			{
-				if (buttons[position.row][position.colorCol + 1].ToString().Any())
+				if (buttons[position.row][position.colorCol + 1].Content.ToString().Any())
+				{
 					position.colorCol++;
+					MoveColorMarker(position.row, position.colorCol - 1);
+				}
 			}
 		}
 
@@ -213,6 +393,51 @@ namespace Wordle_Helper
 			return runArgs;
 		}
 
+		private async void RunRow(object sender, RoutedEventArgs e)
+		{
+			if (0 != states[position.row][position.colorCol])   //Advance colorCol if current letter has been evaluated
+			{
+				ColorPosRight();
+			}
+
+			if (maxCol == position.col)     //Submit word
+			{
+				//Ensure every letter in row has a state
+				if (false == StatesAssigned())
+					return;
+
+				//Gray out submit button
+				GrayOut(SubmitButtons[position.row]);
+				HideBorders();
+				await Task.Run(() => Task.Delay(1));
+
+				//update wordle analyzer with row word and values
+				var args = GetRunArgs();
+				var result = an.Run(args);
+				NextRow();
+			}
+			else if (0 == position.col)     //Suggest word
+			{
+				//Gray out suggest button
+				GrayOut(SubmitButtons[position.row]);
+				HideBorders();
+				await Task.Run(() => Task.Delay(1));
+
+				//get word suggestion by running analyzer and search
+				var result = an.Run(null);
+				string suggestion = new Search(result, connectionString, tableString, columnString).Run();
+				for (int i = 0; i < maxCol; i++)
+				{
+					buttons[position.row][i].Content = char.ToUpper(suggestion[i]);
+				}
+				position.col = maxCol;
+
+				DisableSubmit();
+			}
+			else
+				return;
+		}
+
 		private void HandleKeyPress(object sender, KeyEventArgs e)
 		{
 			string key = e.Key.ToString();
@@ -224,38 +449,7 @@ namespace Wordle_Helper
 			//handle enter and backspace
 			if ("Return" == key)
 			{
-				if (0 != states[position.row][position.colorCol])   //Advance colorCol if current letter has been evaluated
-				{
-					ColorPosRight();
-				}
-
-				if (maxCol == position.col)		//Submit word
-				{
-					//Ensure every letter in row has a state
-					foreach (short state in states[position.row])
-					{
-						if (0 == state)
-							return; //At least one letter does not have a state
-					}
-
-					//update wordle analyzer with row word and values
-					var args = GetRunArgs();
-					var result = an.Run(args);
-					NextRow();
-				}
-				else if (0 == position.col)		//Suggest word
-				{
-					//get word suggestion by running analyzer and search
-					var result = an.Run(null);
-					string suggestion = new Search(result, connectionString, tableString, columnString).Run();
-					for (int i = 0; i < maxCol; i++)
-					{
-						buttons[position.row][i].Content = char.ToUpper(suggestion[i]);
-					}
-					position.col = maxCol;
-				}
-				else
-					return;
+				RunRow(sender, e);
 			}
 			if ("Back" == key)
 			{
@@ -269,14 +463,22 @@ namespace Wordle_Helper
 			if ("" == buttons[position.row][col].Content.ToString())
 				col--;
 
+			if("Escape" == key)
+			{
+				RemoveColorHighlight();
+				return;
+			}
+
 			if ("Up" == key)
 			{
 				ToggleButton(buttons[position.row][position.colorCol], 1);
+				HighlightColorPos();
 				return;
 			}
 			if ("Down" == key)
 			{
 				ToggleButton(buttons[position.row][position.colorCol], -1);
+				HighlightColorPos();
 				return;
 			}
 			if("Left" == key)
